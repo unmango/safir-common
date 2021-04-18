3c8abbc8a821f01fa5151784a398af130be1ee36
Erik Rasmussen <ecr.referee@hotmail.com>
2021-04-18 15:39:49 -0500

    Add more projects

---
+++ src/Safir.Messaging.MediatR/EventExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MediatR;
+using Safir.Messaging.Abstractions;
+
+namespace Safir.Messaging.MediatR
+{
+    internal record NotificationWrapper<T>(T Value) : INotification where T : IEvent;
+    
+    public static class EventExtensions
+    {
+        public static INotification AsNotification<T>(this T @event)
+            where T : IEvent
+        {
+            return new NotificationWrapper<T>(@event);
+        }
+
+        public static T GetEvent<T>(this INotification notification)
+            where T : IEvent
+        {
+            if (notification is not NotificationWrapper<T> wrapper)
+            {
+                throw new NotSupportedException("Unable to unwrap notification");
+            }
+
+            return wrapper.Value;
+        }
+    }
+}
