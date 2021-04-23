b96d3d0a7e6e86d2e6375319ba71cdbce79be156
Erik Rasmussen <ecr.referee@hotmail.com>
2021-04-22 20:52:19 -0500

    Trying to get source generators working

---
+++ test/Safir.Messaging.MediatR.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Safir.Messaging.MediatR.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Some BULLSHIT");
+        }
+    }
+}
