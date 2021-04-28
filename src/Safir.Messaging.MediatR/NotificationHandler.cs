1bd58d61dfed2c64eae73654b4cd224b5e10dada
Erik Rasmussen <ecr.referee@hotmail.com>
2021-04-27 22:03:20 -0500

    Add abstract handle for unwrapping notification

---
+++ src/Safir.Messaging.MediatR/NotificationHandler.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Safir.Messaging.MediatR
+{
+    public abstract class NotificationHandler<T> : INotificationHandler<Notification<T>>
+        where T : IEvent
+    {
+        Task INotificationHandler<Notification<T>>.Handle(
+            Notification<T> notification,
+            CancellationToken cancellationToken)
+        {
+            return Handle(notification.Value, cancellationToken);
+        }
+
+        protected abstract Task Handle(T notification, CancellationToken cancellationToken);
+    }
+}
