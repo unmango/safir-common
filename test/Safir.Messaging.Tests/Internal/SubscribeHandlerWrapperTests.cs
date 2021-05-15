1ec849b3ca4108421e2cf6303051c8d3f8b39603
Erik Rasmussen <ecr.referee@hotmail.com>
2021-05-15 17:18:02 -0500

    WIP adding subscribe wrapper tests

---
+++ test/Safir.Messaging.Tests/Internal/SubscribeHandlerWrapperTests.cs
@@ -0,0 +1,9 @@
+using Moq.AutoMock;
+
+namespace Safir.Messaging.Tests.Internal
+{
+    public class SubscribeHandlerWrapperTests
+    {
+        private readonly AutoMocker _mocker = new();
+    }
+}
