8dfaa7661110bf2abd14741a6b04807b166e8b5c
Erik Rasmussen <ecr.referee@hotmail.com>
2021-05-18 22:00:41 -0500

    WIP on maybe scoping things idfk

--- src/Safir.Messaging/SubscriptionManager.cs
+++ src/Safir.Messaging/SubscriptionManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -9,17 +10,17 @@ namespace Safir.Messaging
 {
     internal sealed class SubscriptionManager : IHostedService
     {
-        private readonly IEnumerable<IEventHandler> _handlers;
+        private readonly IServiceProvider _services;
         private readonly IEventBus _eventBus;
         private readonly ILogger<SubscriptionManager> _logger;
         private readonly List<IDisposable> _subscriptions = new();
 
         public SubscriptionManager(
-            IEnumerable<IEventHandler> handlers,
+            IServiceProvider services,
             IEventBus eventBus,
             ILogger<SubscriptionManager> logger)
         {
-            _handlers = handlers ?? throw new ArgumentNullException(nameof(handlers));
+            _services = services ?? throw new ArgumentNullException(nameof(services));
             _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
             _logger = logger;
         }
@@ -27,25 +28,33 @@ namespace Safir.Messaging
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogDebug("Starting subscription manager");
-            foreach (var group in _handlers.GroupByEvent())
-            {
-                _logger.LogTrace("Subscribing handlers for event type {Type}", group.Key);
-                foreach (var handler in group)
-                {
-                    try
-                    {
-                        var subscription = _eventBus.SubscribeRetry(group.Key, handler, e => {
-                            _logger.LogError(e, "Exception in handler");
-                        });
+            using var scope = _services.CreateScope();
+            var handlers = scope.ServiceProvider.GetRequiredService<IEnumerable<IEventHandler>>();
 
-                        _subscriptions.Add(subscription);
-                    }
-                    catch (Exception e)
-                    {
-                        _logger.LogError(e, "Exception while subscribing handler");
-                    }
-                }
+            foreach (var type in handlers.GetEventTypes())
+            {
+                
             }
+            
+            // foreach (var group in handlers.GroupByEvent())
+            // {
+            //     _logger.LogTrace("Subscribing handlers for event type {Type}", group.Key);
+            //     foreach (var handler in group)
+            //     {
+            //         try
+            //         {
+            //             var subscription = _eventBus.SubscribeRetry(group.Key, handler, e => {
+            //                 _logger.LogError(e, "Exception in handler");
+            //             });
+            //
+            //             _subscriptions.Add(subscription);
+            //         }
+            //         catch (Exception e)
+            //         {
+            //             _logger.LogError(e, "Exception while subscribing handler");
+            //         }
+            //     }
+            // }
 
             _logger.LogTrace("Exiting StartAsync");
             return Task.CompletedTask;
