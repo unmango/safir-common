59ff5fe37340b1dea9d59f640753a1984a831227
Erik Rasmussen <ecr.referee@hotmail.com>
2021-05-24 00:09:50 -0500

    Add event entity type configuration

--- src/Safir.EventSourcing.EntityFrameworkCore/EventConfiguration.cs
+++ src/Safir.EventSourcing.EntityFrameworkCore/EventConfiguration.cs
@@ -5,11 +5,20 @@ namespace Safir.EventSourcing.EntityFrameworkCore
 {
     public class EventConfiguration : IEntityTypeConfiguration<Event>
     {
-        public void Configure(EntityTypeBuilder<Event> builder)
+        public virtual void Configure(EntityTypeBuilder<Event> builder)
         {
-            builder.Property(x => x.Id)
-                .IsRequired()
-                .ValueGeneratedOnAdd();
+            builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.Type).IsRequired();
+            builder.Property(x => x.AggregateId).IsRequired();
+            builder.Property(x => x.Position).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.Occurred).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.Data).IsRequired();
+            builder.Property(x => x.Metadata).IsRequired();
+            builder.Property(x => x.Version).IsRequired();
+
+            builder.HasKey(x => new { x.Id, x.Type, x.AggregateId });
+            builder.HasDiscriminator(x => x.Type); // Necessary? Detrimental?
+            builder.OwnsOne(x => x.Metadata);
         }
     }
 }
