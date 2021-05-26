efd8dc9b1085d4814969a53b6a7dec922317b8e2
Erik Rasmussen <ecr.referee@hotmail.com>
2021-05-25 22:29:14 -0500

    Refactor event metadata provider

---
+++ src/Safir.EventSourcing.Abstractions/DefaultEventMetadataProvider.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Safir.Messaging;
+
+namespace Safir.EventSourcing
+{
+    [UsedImplicitly]
+    public sealed class DefaultEventMetadataProvider : IEventMetadataProvider
+    {
+        public string GetTypeDiscriminator<T>(T @event, int version) where T : IEvent => typeof(T).AssemblyQualifiedName;
+
+        public ValueTask<string> GetTypeDiscriminatorAsync<T>(
+            T @event,
+            int version,
+            CancellationToken cancellationToken = default) where T : IEvent
+        {
+            return new(GetTypeDiscriminator(@event, version));
+        }
+
+        public Type GetType(string discriminator, int version)
+        {
+            if (string.IsNullOrWhiteSpace(discriminator))
+                throw new ArgumentException("Invalid discriminator", nameof(discriminator));
+
+            var result = Type.GetType(discriminator);
+
+            return result ?? throw new InvalidOperationException("Unable to resolve type from discriminator");
+        }
+
+        public ValueTask<Type> GetTypeAsync(string discriminator, int version, CancellationToken cancellationToken = default)
+        {
+            return new(GetType(discriminator, version));
+        }
+    }
+}
