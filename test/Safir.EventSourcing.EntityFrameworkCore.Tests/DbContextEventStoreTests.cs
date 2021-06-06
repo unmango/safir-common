b37bdf6d62a8c403f734e4c842562a66c9c27b36
Erik Rasmussen <ecr.referee@hotmail.com>
2021-06-05 21:09:51 -0500

    Cleanup usings

--- test/Safir.EventSourcing.EntityFrameworkCore.Tests/DbContextEventStoreTests.cs
+++ test/Safir.EventSourcing.EntityFrameworkCore.Tests/DbContextEventStoreTests.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using Moq.AutoMock;
 using Safir.Messaging;
