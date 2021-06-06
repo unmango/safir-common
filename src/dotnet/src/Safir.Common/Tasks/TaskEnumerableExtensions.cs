eebe7774f4076a343abeff9a87164b4658313bff
Erik Rasmussen <ecr.referee@hotmail.com>
2021-06-05 23:41:18 -0500

    Marten adapter work

--- src/dotnet/src/Safir.Common/Tasks/TaskEnumerableExtensions.cs
+++ src/dotnet/src/Safir.Common/Tasks/TaskEnumerableExtensions.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 
@@ -9,6 +10,30 @@ namespace Safir.Common.Tasks
     [PublicAPI]
     public static class TaskEnumerableExtensions
     {
+        public static async IAsyncEnumerable<T> AsAsyncEnumerable<T>(
+            this Task<IEnumerable<T>> task,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken = default)
+        {
+            foreach (var item in await task)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return item;
+            }
+        }
+
+        public static async IAsyncEnumerable<T> AsAsyncEnumerable<T>(
+            this Task<IReadOnlyList<T>> task,
+            [EnumeratorCancellation]
+            CancellationToken cancellationToken = default)
+        {
+            foreach (var item in await task)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return item;
+            }
+        }
+        
         public static TaskAwaiter<T[]> GetAwaiter<T>(this IEnumerable<Task<T>> enumerable)
         {
             return Task.WhenAll(enumerable).GetAwaiter();
