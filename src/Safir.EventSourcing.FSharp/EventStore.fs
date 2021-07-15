29ae5eb766bc8f5383ad24f7180b27e32ed6bfea
Erik Rasmussen <ecr.referee@hotmail.com>
2021-07-14 22:20:44 -0500

    Auto-implement event store interface

---
+++ src/Safir.EventSourcing.FSharp/EventStore.fs
@@ -0,0 +1,39 @@
+﻿namespace Safir.EventSourcing.FSharp
+
+open Safir.EventSourcing
+
+type EventStore() =
+    interface IEventStore with
+        member this.AddAsync<'TAggregateId>
+            (
+                aggregateId: 'TAggregateId,
+                event: Safir.Messaging.IEvent,
+                cancellationToken: System.Threading.CancellationToken
+            ) : System.Threading.Tasks.Task =
+            failwith "todo"
+
+        member this.AddAsync<'TAggregateId>
+            (
+                aggregateId: 'TAggregateId,
+                events: System.Collections.Generic.IEnumerable<Safir.Messaging.IEvent>,
+                cancellationToken: System.Threading.CancellationToken
+            ) : System.Threading.Tasks.Task =
+            failwith "todo"
+
+        member this.GetAsync<'TAggregateId>
+            (
+                id: System.Guid,
+                cancellationToken: System.Threading.CancellationToken
+            ) : System.Threading.Tasks.Task<Safir.Messaging.IEvent> =
+            failwith "todo"
+
+        member this.GetAsync<'TEvent, 'TAggregateId when 'TEvent :> Safir.Messaging.IEvent>
+            (
+                id: System.Guid,
+                cancellationToken: System.Threading.CancellationToken
+            ) : System.Threading.Tasks.Task<'TEvent> =
+            failwith "todo"
+
+        member this.StreamAsync(aggregateId, startPosition, endPosition, cancellationToken) = failwith "todo"
+        
+        member this.StreamBackwardsAsync(aggregateId, count, cancellationToken) = failwith "todo"
--- src/Safir.EventSourcing.FSharp/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Safir.EventSourcing.FSharp
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
