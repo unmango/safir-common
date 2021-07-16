65e70e06b0bcdb93a86642322ab59ae76a8c6c5a
Erik Rasmussen <ecr.referee@hotmail.com>
2021-07-15 21:03:10 -0500

    WIP implementing store

--- src/Safir.EventSourcing.FSharp/EventStore.fs
+++ src/Safir.EventSourcing.FSharp/EventStore.fs
@@ -1,39 +1,53 @@
 ﻿namespace Safir.EventSourcing.FSharp
 
+open System;
+open System.Threading
+open System.Threading.Tasks
 open Safir.EventSourcing
+open Safir.Messaging;
 
+[<AbstractClass>]
 type EventStore() =
+    abstract AddAsync<'TAggregateId> : 'TAggregateId -> IEvent -> CancellationToken -> Task;
+        
+    abstract GetAsync<'TAggregateId> : Guid -> CancellationToken -> Task<IEvent>;
+
     interface IEventStore with
         member this.AddAsync<'TAggregateId>
             (
                 aggregateId: 'TAggregateId,
-                event: Safir.Messaging.IEvent,
-                cancellationToken: System.Threading.CancellationToken
-            ) : System.Threading.Tasks.Task =
-            failwith "todo"
+                event: IEvent,
+                cancellationToken: CancellationToken
+            ) : Task =
+            this.AddAsync aggregateId event cancellationToken
 
         member this.AddAsync<'TAggregateId>
             (
                 aggregateId: 'TAggregateId,
-                events: System.Collections.Generic.IEnumerable<Safir.Messaging.IEvent>,
-                cancellationToken: System.Threading.CancellationToken
-            ) : System.Threading.Tasks.Task =
-            failwith "todo"
+                events: System.Collections.Generic.IEnumerable<IEvent>,
+                cancellationToken: CancellationToken
+            ) : Task =
+            // TODO: Learn F# so I can inline this
+            let addAsync x =
+                this.AddAsync aggregateId x cancellationToken
+
+            Task.WhenAll(Seq.map addAsync events)
 
         member this.GetAsync<'TAggregateId>
             (
-                id: System.Guid,
-                cancellationToken: System.Threading.CancellationToken
-            ) : System.Threading.Tasks.Task<Safir.Messaging.IEvent> =
-            failwith "todo"
+                id: Guid,
+                cancellationToken: CancellationToken
+            ) : Task<IEvent> =
+            this.GetAsync<'TAggregateId> id cancellationToken
 
-        member this.GetAsync<'TEvent, 'TAggregateId when 'TEvent :> Safir.Messaging.IEvent>
+        member this.GetAsync<'TEvent, 'TAggregateId when 'TEvent :> IEvent>
             (
-                id: System.Guid,
-                cancellationToken: System.Threading.CancellationToken
-            ) : System.Threading.Tasks.Task<'TEvent> =
+                id: Guid,
+                cancellationToken: CancellationToken
+            ) : Task<'TEvent> =
+            // this.GetAsync<'TAggregateId> id cancellationToken
             failwith "todo"
-
+            
         member this.StreamAsync(aggregateId, startPosition, endPosition, cancellationToken) = failwith "todo"
-        
+
         member this.StreamBackwardsAsync(aggregateId, count, cancellationToken) = failwith "todo"
