5732391f6d30a2210416258477435c4d17e99263
Erik Rasmussen <ecr.referee@hotmail.com>
2021-07-20 21:23:38 -0500

    Little bits and pieces

--- src/node/packages/agent-client/AgentClient.ts
+++ src/node/packages/agent-client/AgentClient.ts
@@ -1,5 +1,13 @@
-import { FileSystemClient } from '@safir/protos/agent';
+import { FileSystemClient } from '@safir/protos/dist/agent';
 
 export default class AgentClient {
+
+  private readonly _baseUrl: string;
   private readonly _files: FileSystemClient;
+
+  constructor(baseUrl: string) {
+    this._baseUrl = baseUrl;
+    this._files = new FileSystemClient(baseUrl);
+  }
+
 }
