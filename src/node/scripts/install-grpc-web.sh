0fa0992fb638992aa682b63cbb019cb781f78a14
Erik Rasmussen <ecr.referee@hotmail.com>
2021-07-25 12:57:48 -0500

    Protoc and grpc-web setup

---
+++ src/node/scripts/install-grpc-web.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+VER=$(curl -s "https://api.github.com/repos/grpc/grpc-web/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")');
+NAME="protoc-gen-grpc-web-$VER-linux-x86_64";
+URL="https://github.com/grpc/grpc-web/releases/download/$VER/$NAME";
+BINDIR="$(dirname $0)/.bin";
+
+if [ ! -d "$BINDIR" ]; then
+    mkdir $BINDIR;
+fi
+
+BIN="$BINDIR/$NAME";
+curl -sSL -o $BIN $URL
+chmod +x $BIN
+export PATH="$PATH:$BINDIR"
