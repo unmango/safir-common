483eb929e7b0e096b559e114a4e95e478bda4f0f
Erik Rasmussen <ecr.referee@hotmail.com>
2021-07-25 13:22:18 -0500

    Resolve full path

--- src/node/scripts/install-grpc-web.sh
+++ src/node/scripts/install-grpc-web.sh
@@ -5,7 +5,8 @@ set -e
 VER=$(curl -s "https://api.github.com/repos/grpc/grpc-web/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")');
 NAME="protoc-gen-grpc-web-$VER-linux-x86_64";
 URL="https://github.com/grpc/grpc-web/releases/download/$VER/$NAME";
-BINDIR="$(dirname $0)/.bin";
+RELBINDIR="$(dirname $0)/.bin";
+BINDIR="$(realpath $RELBINDIR)";
 BIN="$BINDIR/$NAME";
 
 echo "VER: $VER"
