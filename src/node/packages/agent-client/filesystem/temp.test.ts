72945bedd69eea914a18021607cbdbc32d6c7c5d
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-03 22:32:10 -0500

    WIP on just BS

--- src/node/packages/agent-client/filesystem/temp.test.ts
+++ src/node/packages/agent-client/filesystem/temp.test.ts
@@ -1,8 +1,25 @@
-import { firstValueFrom } from 'rxjs';
+global.XMLHttpRequest = require('xhr2');
+
+import { FileSystemClient } from '@unmango/safir-protos/dist/agent';
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+import { firstValueFrom, timeout } from 'rxjs';
 import { list } from './list';
 
 test('calls agent', async () => {
-  const result$ = list('localhost:6969');
-  const result = await firstValueFrom(result$)
-  console.log(result);
-});
+  const client = new FileSystemClient('localhost:6969');
+
+  const stream = client.list(new Empty());
+  stream.on('data', x => console.log(x));
+  stream.on('error', e => console.error(e));
+  stream.on('end', () => console.log('end'));
+
+  await new Promise(res => {
+    setTimeout(res, 20000);
+  });
+
+  // const result$ = list('localhost:6969');
+  // const result = await firstValueFrom(result$.pipe(
+  //   timeout({ each: 60000 }),
+  // ));
+  // console.log(result);
+}, 60000);
