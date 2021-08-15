8697baaf5ebb6c4ef5b4c1c76dc1c97d4058657a
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-15 18:52:09 -0500

    Refactor

--- src/node/packages/grpc-client/index.ts
+++ src/node/packages/grpc-client/index.ts
@@ -1,62 +1,40 @@
 import { FileSystemClient } from '@unmango/safir-protos/dist/agent';
 import { HostClient } from '@unmango/safir-protos/dist';
-import { ClientReadableStream, Error, Metadata } from 'grpc-web';
+import { ClientReadableStream, Metadata } from 'grpc-web';
 import { Observable } from 'rxjs';
 
 type ServerStreaming<Request, Response> = {
   (request: Request, metadata?: Metadata): ClientReadableStream<Response>;
 };
 
-type ServerStreamingProperties<T> = {
-  [P in keyof T as T[P] extends ServerStreaming<unknown, unknown> ? P : never]: T[P];
-};
-
-type ObservableStream<Request, Response> = {
-  (request: Request, metadata?: Metadata): Observable<Response>;
-};
-
-type AsObservableStream<T> = T extends ServerStreaming<infer Request, infer Response>
-  ? ObservableStream<Request, Response>
-  : never;
-
 type ObservableStreamProperties<T> = {
-  [P in keyof ServerStreamingProperties<T>]: AsObservableStream<ServerStreamingProperties<T>[P]>;
+  [P in keyof T as T[P] extends ServerStreaming<unknown, unknown> ? P : never]:
+    T[P] extends ServerStreaming<infer Req, infer Res>
+      ? (request: Req, metadata?: Metadata) => Observable<Res>
+      : never;
 };
 
-type Test = ObservableStreamProperties<FileSystemClient>;
-
-const test: Test = {
-}
-
-type AsyncStream<Request, Response> = {
-  (request: Request, metadata?: Metadata): Promise<Response[]>;
-};
-
-type AsAsyncStream<T> = T extends ServerStreaming<infer Request, infer Response>
-  ? AsyncStream<Request, Response>
-  : never;
-
-type AppendAsync<T> =
-  T extends string ? `${T}Async` :
-  { [P in keyof T as AppendAsync<P>]: T[P] };
+type AppendAsync<T extends string> = `${T & string}Async`;
 
 type AsyncStreamProperties<T> = {
-  [P in keyof T as keyof AppendAsync<ServerStreamingProperties<T>>]: AsAsyncStream<T[P]>;
+  [P in keyof T as T[P] extends ServerStreaming<unknown, unknown> ? AppendAsync<P & string> : never]:
+    T[P] extends ServerStreaming<infer Req, infer Res>
+      ? (request: Req, metadata?: Metadata) => Promise<Res[]>
+      : never;
 };
 
 type Unary<Request, Response> = {
   (request: Request, metadata: Metadata | null): Promise<Response>;
 };
 
-type UnaryProperties<T> = {
-  [P in keyof T as T[P] extends Unary<unknown, unknown> ? P : never]: T[P];
-};
-
 type AsyncUnaryProperties<T> = {
-  [P in keyof T as keyof AppendAsync<UnaryProperties<T>>]: T[P]
+  [P in keyof T as T[P] extends Unary<unknown, unknown> ? AppendAsync<P & string> : never]:
+    T[P] extends Unary<infer Req, infer Res>
+      ? (request: Req, metadata?: Metadata) => Promise<Res>
+      : never;
 };
 
-// TODO: Why does it use client_ as the hint prop name?
+// TODO: Why doesn't it append `Async` in the hint text?
 export type GrpcClient<T> =
   & ObservableStreamProperties<T>
   & AsyncStreamProperties<T>
